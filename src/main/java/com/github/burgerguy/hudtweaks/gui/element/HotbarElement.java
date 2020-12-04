d8c0488ad6b9caf36c7894ab5e53255f6a764e3d
burgerguy <ryan.gehling@yahoo.com>
2020-12-04 10:50:49 -0500

    change hotbarwidget to work with offhand and attack indicator

--- src/main/java/com/github/burgerguy/hudtweaks/gui/element/HotbarElement.java
+++ src/main/java/com/github/burgerguy/hudtweaks/gui/element/HotbarElement.java
@@ -6,17 +6,33 @@ import com.github.burgerguy.hudtweaks.gui.HudElement;
 import com.github.burgerguy.hudtweaks.util.gui.MatrixCache.UpdateEvent;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.AttackIndicator;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class HotbarElement extends HudElement {
 
 	public HotbarElement() {
 		super("hotbar", UpdateEvent.ON_SCREEN_BOUNDS_CHANGE);
 	}
+	
+	private int getAttackIndicatorOffset(MinecraftClient client) {
+		return (client != null && client.options.attackIndicator.equals(AttackIndicator.HOTBAR)) ? 24 : 0;
+	}
+	
+	private int getOffhandOffset(MinecraftClient client) {
+		Entity cameraEntity = client.getCameraEntity();
+		if (cameraEntity != null && cameraEntity instanceof PlayerEntity) {
+			if (!((PlayerEntity) cameraEntity).getOffHandStack().isEmpty()) {
+				return 29;
+			}
+		}
+		return 0;
+	}
 
 	@Override
 	public int getWidth(MinecraftClient client) {
-		// FIXME: WORK WITH OFFHAND SLOT AND HOTBAR ATTACK INDICATOR
-		return 182;
+		return 182 + getAttackIndicatorOffset(client) + getOffhandOffset(client);
 	}
 
 	@Override
@@ -26,7 +42,7 @@ public class HotbarElement extends HudElement {
 
 	@Override
 	public Point calculateDefaultCoords(MinecraftClient client) {
-		return new Point((client.getWindow().getScaledWidth() / 2) - 91, client.getWindow().getScaledHeight() - 24);
+		return new Point((client.getWindow().getScaledWidth() / 2) - 91 - getOffhandOffset(client), client.getWindow().getScaledHeight() - 24);
 	}
 	
 }
