f220bc55d85ef259682859badcc452e76f015dda
burgerguy <ryan.gehling@yahoo.com>
2020-12-23 12:30:10 -0500

    be un dumb

--- src/main/java/com/github/burgerguy/hudtweaks/api/CustomHudElement.java
+++ src/main/java/com/github/burgerguy/hudtweaks/api/CustomHudElement.java
@@ -4,7 +4,7 @@ import com.github.burgerguy.hudtweaks.gui.HudContainer;
 import com.github.burgerguy.hudtweaks.gui.HudElement;
 
 public abstract class CustomHudElement extends HudElement {
-	public CustomHudElement(String identifier, String[] updateEvents, MatrixUpdater updater) {
+	public CustomHudElement(String identifier, MatrixUpdater updater, String... updateEvents) {
 		super(identifier, updateEvents);
 		updater.fillRunnables(ms -> HudContainer.getMatrixCache().tryPushMatrix(identifier, ms),
 				ms -> HudContainer.getMatrixCache().tryPopMatrix(identifier, ms));
