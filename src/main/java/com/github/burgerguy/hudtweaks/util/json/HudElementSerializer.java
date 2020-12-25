a2c00662eccabf5aa34ef41b86f2f83a08e6c70d
burgerguy <ryan.gehling@yahoo.com>
2020-12-24 21:08:08 -0500

    stop using JsonPrimitives in serialization

--- src/main/java/com/github/burgerguy/hudtweaks/util/json/HudElementSerializer.java
+++ src/main/java/com/github/burgerguy/hudtweaks/util/json/HudElementSerializer.java
@@ -8,7 +8,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
@@ -19,16 +18,16 @@ public class HudElementSerializer implements JsonSerializer<HudElement> {
 	public JsonElement serialize(HudElement element, Type typeOfSrc, JsonSerializationContext context) {
 		JsonObject xPosObject = new JsonObject();
 		xPosObject.add("posType", context.serialize(element.xPosType));
-		xPosObject.add("parent", new JsonPrimitive(element.getXParent().getIdentifier()));
-		xPosObject.add("anchorPos", new JsonPrimitive(element.xAnchorPos));
-		xPosObject.add("relativePos", new JsonPrimitive(element.xRelativePos));
-		xPosObject.add("offset", new JsonPrimitive(element.xOffset));
+		xPosObject.add("parent", context.serialize(element.getXParent().getIdentifier()));
+		xPosObject.add("anchorPos", context.serialize(element.xAnchorPos));
+		xPosObject.add("relativePos", context.serialize(element.xRelativePos));
+		xPosObject.add("offset", context.serialize(element.xOffset));
 		JsonObject yPosObject = new JsonObject();
 		yPosObject.add("posType", context.serialize(element.yPosType));
-		yPosObject.add("parent", new JsonPrimitive(element.getYParent().getIdentifier()));
-		yPosObject.add("anchorPos", new JsonPrimitive(element.yAnchorPos));
-		yPosObject.add("relativePos", new JsonPrimitive(element.yRelativePos));
-		yPosObject.add("offset", new JsonPrimitive(element.yOffset));
+		yPosObject.add("parent", context.serialize(element.getYParent().getIdentifier()));
+		yPosObject.add("anchorPos", context.serialize(element.yAnchorPos));
+		yPosObject.add("relativePos", context.serialize(element.yRelativePos));
+		yPosObject.add("offset", context.serialize(element.yOffset));
 		JsonObject baseObject = INNER_GSON.toJsonTree(element, TypeToken.of(Object.class).getType()).getAsJsonObject();
 		baseObject.add("xPos", xPosObject);
 		baseObject.add("yPos", yPosObject);
