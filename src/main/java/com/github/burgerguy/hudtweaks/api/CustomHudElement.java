b596cdfc45b0f3620f845d7882e1170bb0ff4034
burgerguy <ryan.gehling@yahoo.com>
2021-02-19 09:07:37 -0500

    fix issue in CustomHudElement

--- src/main/java/com/github/burgerguy/hudtweaks/api/CustomHudElement.java
+++ src/main/java/com/github/burgerguy/hudtweaks/api/CustomHudElement.java
@@ -7,7 +7,7 @@ import com.github.burgerguy.hudtweaks.hud.element.HudElementEntry;
 public abstract class CustomHudElement extends HudElementEntry {
 	public CustomHudElement(HTIdentifier identifier, MatrixUpdater updater, String... updateEvents) {
 		super(identifier, updateEvents);
-		updater.fillRunnables(ms -> HudContainer.getMatrixCache().tryPushMatrix(identifier, ms),
-				ms -> HudContainer.getMatrixCache().tryPopMatrix(identifier, ms));
+		updater.fillRunnables(ms -> HudContainer.getMatrixCache().tryPushMatrix(identifier.getElementType(), ms),
+				ms -> HudContainer.getMatrixCache().tryPopMatrix(identifier.getElementType(), ms));
 	}
 }
