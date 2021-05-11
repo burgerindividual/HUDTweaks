34d9efec97755fc61710430d8ca5989073ffc788
burgerguy <ryan.gehling@yahoo.com>
2021-05-11 10:30:16 -0400

    start making bossbar limit modifiable

---
+++ src/main/java/com/github/burgerguy/hudtweaks/util/hud/StaticBossBarHelper.java
@@ -0,0 +1,7 @@
+package com.github.burgerguy.hudtweaks.util.hud;
+
+public enum StaticBossBarHelper {
+	; // no instantiation, all contents static
+	
+	public static float SCREEN_PERCENT = (1.0f / 3.0f);
+}
